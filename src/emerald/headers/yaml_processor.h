2153eb32c7681af4e61d16439a18a976bd26bf2f
Oblivious <me262thanos@gmail.com>
2021-02-23 21:59:24 +0200

    Made yaml_processor only read the `em.yml` file.

--- src/emerald/headers/yaml_processor.h
+++ src/emerald/headers/yaml_processor.h
@@ -2,25 +2,14 @@
 #define __YAML_PROCESSOR_H_
 
 #include "vector.h"
-
-struct yaml_processor {
-    char *filename;
-};
-
-/**
- * @func: yaml_processor_new
- * @brief Creates a new yaml_processor object
- * @param filename -> A name of the emfile to process
- * @return struct yaml_processor* -> The newly created object
- */
-struct yaml_processor *yaml_processor_new(char *filename);
+#include "string.h"
+#include "read_handler.h"
 
 /**
  * @func: get_dependencies_from_yaml
  * @brief Get the dependencies from yaml file
- * @param y -> The yaml_processor containing the name of the file
  * @return vector* -> The list of dependencies
  */
-vector *get_dependencies_from_yaml(struct yaml_processor *y);
+vector *get_dependencies_from_yaml(void);
 
 #endif
--- src/emerald/yaml_processor.c
+++ src/emerald/yaml_processor.c
@@ -1,12 +1,26 @@
 #include "headers/yaml_processor.h"
+#include "headers/read_handler.h"
 
-struct yaml_processor *yaml_processor_new(char *filename) {
-    struct yaml_processor *y = (struct yaml_processor*)malloc(sizeof(struct yaml_processor));
-    y->filename = filename;
+/* TODO -> ADD AN ACTUAL YAML FILE HANDLER */
 
-    return y;
-}
+vector *get_dependencies_from_yaml(void) {
+    char *line;
+    vector *deps = vector_new();
 
-vector *get_dependencies_from_yaml(struct yaml_processor *y) {
-    return vector_new();
+    struct read_handler *h = read_handler_new();
+    if(!read_handler_open(h, "em.yml")) return deps;
+
+    /* Skip to the dependencies part */
+    while((line = read_handler_read_line(h)))
+        if(!strcmp(line, "dependencies:")) break;
+
+    /* Start reading dependencies */
+    while((line = read_handler_read_line(h))) {
+        string *l = string_new(line);
+        l = string_substring(l, 0, 7);
+        if(!strcmp(string_get(l), "license:")) break;
+        vector_add(deps, string_new(line));
+    }
+
+    return deps;
 }
