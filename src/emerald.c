0543daa37e3315417c7a45beb8f58df79fc00059
Oblivious <me262thanos@gmail.com>
2021-02-23 14:05:05 +0200

    Added a main API of emerald on main.

--- src/emerald.c
+++ src/emerald.c
@@ -1,6 +1,44 @@
 #include "emerald.h"
 
-int main(void) {
-    printf("%s\n", "Hello, World!");
+void print_strings(void *item) {
+    printf("%s\n", (char*)item);
+}
+
+void usage(void) {
+    printf("%s\n", "Usage.");
+    exit(1);
+}
+
+int main(int argc, char **argv) {
+    if(argc < 2) usage();
+
+    char *action = argv[1];
+    if(!strcmp(action, "init")) {
+        if(argc < 3) usage();
+
+        /* Get the third argument */
+        char *name = argv[2];
+        initialize_em_library(name);
+    }
+    else if(!strcmp(action, "list"))
+        vector_map(get_dependencies(), (vector_lambda)print_strings);
+    else if(!strcmp(action, "install"))
+        install_dependencies();
+    else if(!strcmp(action, "build")) {
+        if(argc < 3) usage();
+        
+        char *type = argv[2];
+        if(!strcmp(type, "app"))
+            compile_as_executable();
+        else if(!strcmp(type, "lib"))
+            compile_as_library();
+        else
+            usage();
+    }
+    else if(!strcmp(action, "version"))
+        printf("%s\n", get_em_version());
+    else
+        usage();
+
     return 0;
 }
--- src/emerald.h
+++ src/emerald.h
@@ -5,5 +5,7 @@
 #include "emerald/headers/write_handler.h"
 #include "emerald/headers/string.h"
 #include "emerald/headers/vector.h"
+#include "emerald/headers/command_processor.h"
+#include "emerald/headers/yaml_processor.h"
 
 #endif
