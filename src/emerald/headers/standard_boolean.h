c4aef6a581919a7ceac19bf5dcb8e3035c85c370
Oblivious <me262thanos@gmail.com>
2021-02-26 03:14:23 +0200

    Fixed the typedef bool issue. (C++ linter complaining)

--- src/emerald/headers/standard_boolean.h
+++ src/emerald/headers/standard_boolean.h
@@ -2,7 +2,7 @@
 #define __STANDARD_BOOLEAN_H_
 
 /** @param bool -> A 'big' enough size to hold both 1 and 0 **/
-typedef unsigned char bool;
+#define bool unsigned char
 #define true 1
 #define false 0
 
