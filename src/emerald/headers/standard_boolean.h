df01c663c61b2b497179077edb154c711736d325
Oblivious-Oblivious <me262thanos@gmail.com>
2021-02-28 04:40:15 +0200

    Removed old C legacy code.

--- src/emerald/headers/standard_boolean.h
+++
@@ -1,9 +0,0 @@
-#ifndef __STANDARD_BOOLEAN_H_
-#define __STANDARD_BOOLEAN_H_
-
-/** @param bool -> A 'big' enough size to hold both 1 and 0 **/
-#define bool unsigned char
-#define true 1
-#define false 0
-
-#endif
