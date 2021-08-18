ac1f98e31e2ff42bd6c48b747369526c071acc0d
Oblivious <me262thanos@gmail.com>
2021-08-19 02:16:36 +0300

    Removed the makefile option temporarily.

--- src/emeralds/main.cr
+++ src/emeralds/main.cr
@@ -48,8 +48,9 @@ class Emeralds::Main
             Emeralds::Version.new.run;
         when "clean"
             Emeralds::Clean.new.run;
-        when "makefile"
-            Emeralds::GenerateMakefile.new.run;
+        # TODO Not working for now
+        # when "makefile"
+        #     Emeralds::GenerateMakefile.new.run;
         when "loc"
             if ARGV.size < 2
                 Emeralds::Loc.new.run;
