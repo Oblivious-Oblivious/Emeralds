2c320a6e19ab3094ccb24e5d8c7476e4e6093153
atha <atha@fourth-wall.org>
2026-05-24 12:48:25 +0300

    [master] - allows scoop to filename automatically.

--- scripts/deploy.rb
+++ scripts/deploy.rb
@@ -108,7 +108,6 @@ File.write(
     "license" => "MIT",
     "url" => zip_url,
     "hash" => zip_sha256,
-    "extract_dir" => "Emeralds-#{version}",
     "depends" => ["crystal", "git"],
     "install" => {
       "script" => [
