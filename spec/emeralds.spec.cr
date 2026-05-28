bdc55d5e6355c3c810d28e7c085197aec44830b6
atha <atha@fourth-wall.org>
2026-05-29 02:36:51 +0300

    [QOL-05] - added missing specs.

--- spec/emeralds.spec.cr
+++ spec/emeralds.spec.cr
@@ -9,5 +9,6 @@ require "./IT/full-flow/step3-em-test.spec";
 require "./IT/full-flow/step4-em-add.spec";
 require "./IT/full-flow/step5-em-build.spec";
 require "./IT/full-flow/step6-em-rest-of-calls.spec";
-require "./IT/full-flow/step7-em-reinstall.spec";
+require "./IT/full-flow/step7-em-install-git.spec";
+require "./IT/full-flow/step8-em-reinstall.spec";
 require "./IT/full-flow/step99-cleanup.spec";
